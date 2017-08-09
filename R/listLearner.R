46a3e49449ba225411c070c687a8c0b2652c0355
Jeremy Coyle <jeremyrcoyle@gmail.com>
2017-08-09 13:28:58 -0700

    started learner documentation

--- R/listLearner.R
+++ R/listLearner.R
@@ -1,23 +1,2 @@
-listLearners <- function(what = "both") {
-    if (what == "both") {
-        message("All prediction algorithm wrappers in SuperLearner:\n")
-        print(ls("package:SuperLearner", pattern = "^[S]L"))
-        message("\nAll screening algorithm wrappers in SuperLearner:\n")
-        print("All")
-        print(ls("package:SuperLearner", pattern = "screen"))
-    } else if (what == "SL") {
-        message("All prediction algorithm wrappers in SuperLearner:\n")
-        print(ls("package:SuperLearner", pattern = "^[S]L"))
-    } else if (what == "screen") {
-        message("All screening algorithm wrappers in SuperLearner:\n")
-        print("All")
-        print(ls("package:SuperLearner", pattern = "screen"))
-    } else if (what == "method") {
-        message("All methods in SuperLearner package:\n")
-        print(ls("package:SuperLearner", pattern = "^method"))
-    } else {
-        message("All functions in SuperLearner:\n")
-        print(ls("package:SuperLearner"))
-    }
-    invisible(ls("package:SuperLearner"))
-}
+
+learners=list(CV_Learner,GLM_Learner, fastGLM_Learner, Model_Matrix, SL_Learner, SL_Screener, Super_Learner)
