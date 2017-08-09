358b8534046ff15f615d607e7e3dad561fa4b612
Oleg Sofrygin <olegso@gmail.com>
2017-08-09 14:16:58 -0700

    travis and removing gridisl dependency

--- tests/testthat/test_all_learners.R
+++ tests/testthat/test_all_learners.R
@@ -5,24 +5,24 @@ task2=Learner_Task$new(mtcars,covariates=c("cyl", "disp", "hp", "drat", "wt", "q
 
 
 test_learner=function(learner, task){
-	
+
 	#test learner definition
 	learner_obj=learner$new()
 	print(sprintf("Testing Learner: %s",learner_obj$name))
-	
+
 	#test learner training
-  fit_obj=learner_obj$train(task)	
+  fit_obj=learner_obj$train(task)
   test_that("Learner can be trained on data",expect_true(fit_obj$is_trained))
-  
+
 	#test learner prediction
   train_preds=fit_obj$predict()
   test_that("Learner can generate training set predictions",expect_equal(length(train_preds),nrow(task$X)))
-	
+
   holdout_preds=fit_obj$predict(task2)
   test_that("Learner can generate holdout set predictions",expect_equal(train_preds,holdout_preds))
-  
+
   #test learner chaining
-  
+
 }
 
 test_learner(GLM_Learner, task)
