eb7401722c751b8b487590f53ab950fc746c0853
Jeremy Coyle <jeremyrcoyle@gmail.com>
2017-08-10 13:12:47 -0700

    GLM_Learner was accidentally dropped from the namespace. Exporting it again

--- R/GLM_Learner.R
+++ R/GLM_Learner.R
@@ -1,5 +1,6 @@
 
 #' @importFrom R6 R6Class
+#' @export
 #' @rdname undocumented_learner
 GLM_Learner <- R6Class(classname = "GLM_Learner", inherit = Learner, portable = TRUE, class = TRUE, private = list(.train = function(task) {
     # todo: if possible have this use task$Xmat with glm.fit or speedglm
