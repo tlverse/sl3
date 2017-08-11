cf6e9fdd0ed011162e2681fa5f7d7af8a771ab3f
Oleg Sofrygin <olegso@gmail.com>
2017-08-11 11:47:06 -0700

    adding tests for options

---
+++ tests/testthat/test_options.R
@@ -0,0 +1,18 @@
+context("Test Options")
+library(testthat)
+library(sl3)
+
+test_that("sl3 options work as expected", {
+  sl3:::.onLoad()
+  sl3:::.onAttach()
+
+  sl3Options()
+  sl3Options("sl3.verbose")
+  expect_error(sl3Options("blahblah"))
+  expect_error(sl3Options("blahblah", 5))
+
+  sl3Options("sl3.verbose", TRUE)
+  sl3Options("sl3.verbose")
+  sl3Options("sl3.verbose", FALSE)
+})
+
