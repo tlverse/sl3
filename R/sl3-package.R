a17f3bde434b9759af543e3754d49a53e8208bfc
podTockom <imalenica@berkeley.edu>
2017-08-17 00:41:44 -0700

    Add tests for arima

--- R/sl3-package.R
+++ R/sl3-package.R
@@ -32,3 +32,20 @@ NULL
 #' data(cpp)
 #' head(cpp)
 NULL
+
+#' Bicycle sharing time series dataset  
+#'
+#' @name bsds
+#' @docType data
+#' @description
+#' Bicycle sharing time series dataset from the UCI Machine Learning Repository. 
+#' @source
+#' \url{https://archive.ics.uci.edu/ml/datasets/bike+sharing+dataset}
+#'
+#' Fanaee-T, Hadi, and Gama, Joao, "Event labeling combining ensemble detectors and background knowledge", Progress in Artificial Intelligence (2013): pp. 1-15, Springer Berlin Heidelberg
+#' @usage bsds
+#' @keywords data
+#' @examples
+#' data(bsds)
+#' head(bsds)
+NULL
