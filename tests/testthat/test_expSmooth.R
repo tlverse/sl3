eb7ca9284160fdd57610c29d07d0a61e83a99849
podTockom <imalenica@berkeley.edu>
2017-09-18 00:19:10 -0700

    Add exponential smoothing learner

---
+++ tests/testthat/test_expSmooth.R
@@ -0,0 +1,37 @@
+context("Test Exponential Smoothing sl3")
+
+if (FALSE) {
+  setwd("..")
+  setwd("..")
+  getwd()
+  library("devtools")
+  document()
+  load_all("./")  # load all R files in /R and datasets in /data. Ignores NAMESPACE:
+  # devtools::check() # runs full check
+  setwd("..")
+  install("sl3", build_vignettes = FALSE, dependencies = FALSE)  # INSTALL W/ devtools:
+}
+
+library(testthat)
+library(sl3)
+set.seed(1)
+
+data(bsds)
+covars <- c("cnt")
+outcome <- "cnt"
+
+task <- sl3_Task$new(bsds, covariates = covars, outcome = outcome)
+
+test_that("Automatic Lrnr_expSmooth gives expected values", {
+  expSmooth_learner <- Lrnr_expSmooth$new(n.ahead=5)
+  expSmooth_fit <- expSmooth_learner$train(task)
+  expSmooth_preds <-  expSmooth_fit$predict(task)
+  
+  expSmooth_fit_2 <- forecast::ets(bsds$cnt)
+  expSmooth_preds_2 <- forecast::forecast(expSmooth_fit_2, h = 5)
+  expSmooth_preds_2 <- as.numeric(expSmooth_preds_2$mean)
+  expSmooth_preds_2  <- structure(expSmooth_preds_2, names=1:5)
+
+  expect_true(sum(expSmooth_preds - expSmooth_preds_2) < 10^(-10))
+  expect_true(all.equal(expSmooth_preds_2, expSmooth_preds))
+})
