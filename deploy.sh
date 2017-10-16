0d8ec62ababdb0c1d1e510e019de18f0a5ab7e27
Nima Hejazi <nh@nimahejazi.org>
2017-10-16 12:58:44 -0700

    fix drat auto-deploy feature

--- deploy.sh
+++ deploy.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
-
 set -o errexit -o nounset
-PKG_REPO=$PWD
-PKG_TARBALL=$(ls -1t *.tar.gz | head -n 1)
-cd ..
+addToDrat(){
+  PKG_REPO=$PWD
 
-addToDrat() {
-  mkdir drat; cd drat
+  cd ..; mkdir drat; cd drat
 
   ## Set up Repo parameters
   git init
-  git config user.name "addToDrat"
-  git config user.email "addToDrat@travis.ci"
+  git config user.name "XXX YYY"
+  git config user.email "XXXX@example.com"
+  git config --global push.default simple
 
   ## Get drat repo
-  git remote add upstream \
-    "https://$GH_TOKEN@github.com/jeremyrcoyle/sl3.git" 2>err.txt
-  git fetch upstream gh-pages 2>err.txt
-  git checkout gh-pages 2>err.txt
-  git reset --hard 2>err.txt
+  git remote add upstream "https://$GH_TOKEN@github.com/GITHUB_USERNAME/drat.git"
+  git fetch upstream 2>err.txt
+  git checkout gh-pages
 
   Rscript -e "drat::insertPackage('$PKG_REPO/$PKG_TARBALL', \
     repodir = '.', \
-    commit='Travis publish sl3: build $TRAVIS_COMMIT', \
-    fields='Commit')"
-  git push --force upstream gh-pages 2>err.txt
-}
+    commit='Travis update: build $TRAVIS_BUILD_NUMBER')"
+  git push 2> /tmp/err.txt
 
+}
 addToDrat
 
