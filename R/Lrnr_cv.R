5c3cd085b82d79ea11845e06cfc611156d8b9734
Oleg Sofrygin <olegso@gmail.com>
2017-10-20 13:12:13 -0700

    allow sl with only 1 learner, fix bug

--- R/Lrnr_cv.R
+++ R/Lrnr_cv.R
@@ -5,20 +5,20 @@ safe_dim <- function(x) {
   if (is.null(d)) {
     d <- length(x)
   }
-  
+
   return(d)
 }
 
 
 # sort n-dimensional array (for multinomial/multivariate SL support)
 aorder <- function(mat, index, along = 1) {
-  
+
   dims <- safe_dim(mat)
   args <- ifelse(along == seq_along(dims), "index", "")
   indexer <- paste(c(args, "drop=F"), collapse = ",")
   call <- sprintf("mat[%s]", indexer)
   result <- eval(parse(text = call))
-  
+
   return(result)
 }
 
@@ -70,51 +70,52 @@ Lrnr_cv <- R6Class(classname = "Lrnr_cv",
                          #todo: this breaks if task is delayed
                          folds <- task$folds
                        }
-                       
+
                        learner <- self$params$learner
-                       
+
                        train_task <- function(task, fold){
                          return(task[fold$training_set])
                        }
                        delayed_train_task <- delayed_fun(train_task)
-                       
+
                        delayed_cv_train <- function(fold,learner,task){
                          training_task  <- delayed_train_task(task, fold)
                          training_task$sequential <- TRUE
                          fit_object  <- delayed_learner_train(learner,training_task)
                          return(fit_object)
                        }
-                       
-                       
+
+
                        #todo: maybe write delayed_cross_validate (as it'd be a neat thing to have around anyway)
                        cv_results <- lapply(folds,delayed_cv_train,learner,task)
                        result <- bundle_delayed(cv_results)
-                       
+
                        return(result)
                      },
-                     
+
                      .train = function(task, prefit) {
                        #prefer folds from params, but default to folds from task
                        folds=self$params$folds
                        if(is.null(folds)){
                          folds=task$folds
                        }
-                       
+
                        fold_fits <- prefit
                        learner <- self$params$learner
-                       
+
                        ever_error <- NULL
-                       
+
                        if(inherits(learner,"Stack")){
                          # if we're cross-validating a stack, check for learner errors in any folds and then drop for all folds
                          errored_learners <- sapply(fold_fits, function(fold_fit) fold_fit$fit_object$is_error)
+                         if (is.vector(errored_learners)) errored_learners <- matrix(errored_learners, ncol = length(errored_learners))
                          ever_error <- apply(errored_learners, 1, any)
                          if(any(ever_error)){
                            #drop errored learners from all folds
                            for(fold_fit in fold_fits){
                              fold_fit$update_errors(ever_error)
                            }
-                           
+
                            #warn about dropping
                            errored_learners <- learner$params$learners[ever_error]
                            errored_names <- sapply(errored_learners, `[[`, "name")
@@ -122,12 +123,12 @@ Lrnr_cv <- R6Class(classname = "Lrnr_cv",
                            warning("The following learners failed for one or more folds and will be dropped from all folds: ", all_names)
                          }
                        }
-                       
+
                        fit_object=list(folds = folds, fold_fits = fold_fits, is_error = ever_error)
-                       
+
                        return(fit_object)
                      },
-                     
+
                      .predict = function(task){
                        # if(!identical(task,private$.training_task)){
                        #   stop("task must match training task for Lrnr_cv")
@@ -135,7 +136,7 @@ Lrnr_cv <- R6Class(classname = "Lrnr_cv",
                        #doing train and predict like this is stupid, but that's the paradigm (for now!)
                        folds=private$.fit_object$folds
                        fold_fits = private$.fit_object$fold_fits
-                       
+
                        cv_predict=function(fold,fold_fits,task){
                          validation_task=validation(task)
                          index=validation()
@@ -143,19 +144,18 @@ Lrnr_cv <- R6Class(classname = "Lrnr_cv",
                          predictions=fit$predict(validation_task)
                          list(index=index,predictions=predictions)
                        }
-                       
+
                        # fold_predictions=cross_validate(cv_predict,folds,fold_fits,task, future.globals=F)
                        # don't use cross_validate as it will call future_lapply
                        fold_predictions=lapply(folds,cv_predict, fold_fits, task)
                        index=unlist(lapply(fold_predictions,`[[`,"index"))
                        predictions=data.table::rbindlist(lapply(fold_predictions,`[[`,"predictions"))
                        predictions=aorder(predictions, order(index))
-                       
+
                        return(predictions)
                      },
                      .required_packages = c("origami")
                          )
-                         
+
                    )
-                   
-                   
+
