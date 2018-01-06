137c2fa2dfdb1e73f24718099064f6863299a8ac
Jonathan Levy <jl@jlstiles.com>
2018-01-06 01:53:32 -0800

    hal learner with offset

---
+++ R/Lrnr_hal.R
@@ -0,0 +1,123 @@
+#' Generalized Linear Models
+#'
+#' This learner provides fitting procedures for the highly adaptive lasso
+#'
+#' @docType class
+#'
+#' @importFrom R6 R6Class
+#' @importFrom stats glm predict family
+#'
+#' @export
+#'
+#' @keywords data
+#'
+#' @return Learner object with methods for training and prediction. See
+#'  \code{\link{Lrnr_base}} for documentation on learners.
+#'
+#' @format \code{\link{R6Class}} object.
+#'
+#' @family Learners
+#'
+#' @section Parameters:
+#' \describe{
+#'   \item{\code{...}}{Parameters passed to \code{\link[stats]{glm}}.}
+#' }
+#'
+#' @template common_parameters
+#
+hal <- function(Y,
+                X,
+                newX = NULL,
+                verbose = FALSE,
+                obsWeights = rep(1, length(Y)),
+                nfolds = ifelse(length(Y) <= 100, 20, 10),
+                nlambda = 100,
+                useMin = TRUE,
+                debug = TRUE,
+                parallel = FALSE,
+                family = gaussian(),
+                offset = NULL,
+                ... # allow extra arguments with no death
+)
+Lrnr_glm <- R6Class(
+  classname = "Lrnr_hal", inherit = Lrnr_base,
+  portable = TRUE, class = TRUE,
+  public = list(
+    initialize = function(...) {
+      params <- args_to_list()
+      super$initialize(params = params, ...)
+    }
+  ),
+
+  private = list(
+    .properties = c("continuous", "binomial", "weights", "offset"),
+
+    .train = function(task) {
+      args <- self$params
+      outcome_type <- self$get_outcome_type(task)
+
+      if (is.null(args$family)) {
+        args$family <- outcome_type$glm_family(return_object = TRUE)
+      }
+      family_name <- args$family$family
+      linkinv_fun <- args$family$linkinv
+      # specify data
+      args$X <- as.matrix(task$X)
+      args$Y <- outcome_type$format(task$Y)
+      args$newX = NULL,
+      args$verbose = FALSE,
+      args$obsWeights = rep(1, length(Y)),
+      args$nfolds = ifelse(length(Y) <= 100, 20, 10),
+      args$nlambda = 100,
+      args$useMin = TRUE,
+      args$debug = TRUE,
+      args$parallel = FALSE
+
+      if (task$has_node("weights")) {
+        args$weights <- task$weights
+      }
+
+      if (task$has_node("offset")) {
+        args$offset <- task$offset
+      }
+
+      args$ctrl <- glm.control(trace = FALSE)
+      SuppressGivenWarnings({
+        fit_object <- call_with_args(hal::halplus, args)
+      }, GetWarningsToSuppress())
+
+      fit_object$linear.predictors <- NULL
+      fit_object$weights <- NULL
+      fit_object$prior.weights <- NULL
+      fit_object$y <- NULL
+      fit_object$residuals <- NULL
+      fit_object$fitted.values <- NULL
+      fit_object$effects <- NULL
+      fit_object$qr <- NULL
+      # fit_object$linkinv_fun <- linkinv_fun
+
+      return(fit_object)
+    },
+
+    .predict = function(task = NULL) {
+      verbose <- getOption("sl3.verbose")
+      newX <- as.matrix(task$X)
+      predictions <- rep.int(NA, nrow(X))
+      if (nrow(X) > 0) {
+        if (task$has_node("offset")) {
+          offset = task$offset
+        } else {
+          offset = NULL
+        }
+        predictions <- predict(fit_object,
+                        newdata = newX,
+                        bigDesign = FALSE,
+                        chunks = 10000,
+                        offset = offset)
+
+      }
+      return(predictions)
+    },
+    .required_packages = c("hal")
+  )
+)
