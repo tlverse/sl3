1558d0e347c963fef5a96facd17e731ef0246522
Nima Hejazi <nh@nimahejazi.org>
2018-03-21 01:14:58 -0700

    add working version for Lrnr_hal9001 + elem test

--- R/Lrnr_hal9001.R
+++ R/Lrnr_hal9001.R
@@ -1,11 +1,14 @@
 #' The Scalable Highly Adaptive LASSO
 #'
-#' The procedure uses a custom C++ implementation to generate a design
-#'  matrix (consisting of basis functions corresponding to covariates and
-#'  interactions of covariates) and remove duplicate columns of indicators. The
-#'  LASSO regression is fit to this (usually) very wide matrix using either a
-#'  custom implementation (based on the \code{origami} package) or by a call to
-#'  \code{cv.glmnet} from the \code{glmnet} package.
+#' The Highly Adaptive LASSO is an estimation procedure that generates a design
+#'  matrix consisting of basis functions corresponding to covariates and
+#'  interactions of covariates and fits LASSO regression to this (usually) very
+#'  wide matrix, recovering a nonparametric functional form that describes the
+#'  target prediction function as a composition of subset functions with finite
+#'  variation norm. This implementation uses the \code{hal9001} R package, which
+#'  provides both a custom implementation (based on the \code{origami} package)
+#'  of the CV-LASSO as well the standard call to \code{cv.glmnet} from the
+#'  \code{glmnet} package.
 #'
 #' @docType class
 #' @importFrom R6 R6Class
@@ -13,7 +16,7 @@
 #' @export
 #'
 #' @keywords data
-#' 
+#'
 #' @return Learner object with methods for training and prediction. See
 #'  \code{\link{Lrnr_base}} for documentation on learners.
 #'
@@ -50,61 +53,32 @@
 Lrnr_hal9001 <- R6Class(
   classname = "Lrnr_hal9001", inherit = Lrnr_base,
   portable = TRUE, class = TRUE,
-  # Above, you should change Lrnr_template (in both the object name and the classname argument)
-  # to a name that indicates what your learner does
   public = list(
-    # you can define default parameter values here
-    # if possible, your learner should define defaults for all required parameters
     initialize = function(degrees = NULL,
-                          fit_type = c("glmnet", "lassi"),
+                          fit_type = "glmnet",
                           n_folds = 10,
                           use_min = TRUE,
                           ...) {
-      # this captures all parameters to initialize and saves them as self$params
       params <- args_to_list()
       super$initialize(params = params, ...)
-    },
-
-    # you can define public functions that allow your learner to do special things here
-    # for instance glm learner might return prediction standard errors
-    #special_function = function(arg_1) {
-    #}
+    }
   ),
   private = list(
-    # list properties your learner supports here.
-    # Use sl3_list_properties() for a list of options
     .properties = c("continuous", "binomial"),
 
-    # list any packages required for your learner here.
-    .required_packages = c("hal9001"),
-
-    # .train takes task data and returns a fit object that can be used to generate predictions
     .train = function(task) {
-      # generate an argument list from the parameters that were
-      # captured when your learner was initialized.
-      # this allows users to pass arguments directly to your ml function
       args <- self$params
 
-      # get outcome variable type
-      # prefering learner$params$outcome_type first, then task$outcome_type
       outcome_type <- self$get_outcome_type(task)
-      # should pass something on to your learner indicating outcome_type
-      # e.g. family or objective
-      if (is.null(args$family)) {
-        args$family <- outcome <- type$glm <- family(return <- object = TRUE)
-      }
-      family_name <- args$family$family
-      linkinv_fun <- args$family$linkinv
-      link_fun <- args$family$linkfun
 
-      # add task data to the argument list
-      # what these arguments are called depends on the learner you are wrapping
-      args$x <- as.matrix(task$X_intercept)
-      args$y <- outcome_type$format(task$Y)
+      if (is.null(args$family)) {
+        args$family <- outcome_type$glm_family(return_object = TRUE)$family
+      }
+
+      args$X <- as.matrix(task$X)
+      args$Y <- outcome_type$format(task$Y)
       args$yolo <- FALSE
 
-      # only add arguments on weights and offset
-      # if those were specified when the task was generated
       if (task$has_node("weights")) {
         args$weights <- task$weights
       }
@@ -113,30 +87,14 @@ Lrnr_hal9001 <- R6Class(
         args$offset <- task$offset
       }
 
-      # call a function that fits your algorithm
-      # with the argument list you constructed
-      fit_object <- call_with_args(my_ml_fun, args)
-
-      # return the fit object, which will be stored
-      # in a learner object and returned from the call
-      # to learner$predict
+      fit_object <- call_with_args(hal9001::fit_hal, args)
       return(fit_object)
     },
-
-    # .predict takes a task and returns predictions from that task
     .predict = function(task = NULL) {
-      #self$training_task
-      #self$training_outcome_type
-      #self$fit_object
-
-      X <- task$X_intercept
-      predictions <- rep.int(NA, nrow(X))
-      if (nrow(X) > 0) {
-        predictions <- stats::predict(object = self$fit_object, new_data = X)
-      }
-      predictions <- predict(self$fit_object, task$X)
+      predictions <- predict(self$fit_object, new_data = as.matrix(task$X))
       return(predictions)
-    }
+    },
+    .required_packages = c("hal9001")
   )
 )
 
