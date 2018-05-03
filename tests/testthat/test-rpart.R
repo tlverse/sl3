954ace051030fce9af7b3579044947cc162a8d8d
Nima Hejazi <nh@nimahejazi.org>
2018-05-02 21:39:24 -0700

    update docs and clean up rpart test

--- tests/testthat/test-rpart.R
+++ tests/testthat/test-rpart.R
@@ -1,4 +1,4 @@
-context("test_Lrnr_rpart.R -- General testing for Rpart")
+context("test-Lrnr_rpart.R -- General testing for Rpart")
 
 library(sl3)
 library(testthat)
@@ -9,6 +9,7 @@ task <- sl3_Task$new(mtcars, covariates = c(
   "cyl", "disp", "hp", "drat", "wt", "qsec",
   "vs", "am", "gear", "carb"
 ), outcome = "mpg")
+
 task2 <- sl3_Task$new(mtcars, covariates = c(
   "cyl", "disp", "hp", "drat", "wt", "qsec",
   "vs", "am", "gear", "carb"
@@ -17,8 +18,8 @@ task2 <- sl3_Task$new(mtcars, covariates = c(
 interactions <- list(c("cyl", "disp"), c("hp", "drat"))
 task_with_interactions <- task$add_interactions(interactions)
 task2 <- task2$add_interactions(interactions)
+
 test_learner <- function(learner, task, ...) {
-  
   # test learner definition this requires that a learner can be instantiated with
   # only default arguments. Not sure if this is a reasonable requirement
   learner_obj <- learner$new(...)
@@ -26,20 +27,20 @@ test_learner <- function(learner, task, ...) {
   # test learner training
   fit_obj <- learner_obj$train(task)
   test_that("Learner can be trained on data", expect_true(fit_obj$is_trained))
-  
+
   # test learner prediction
   train_preds <- fit_obj$predict()
   test_that("Learner can generate training set predictions", expect_equal(
     sl3:::safe_dim(train_preds)[1],
     length(task$Y)
   ))
-  
+
   holdout_preds <- fit_obj$predict(task2)
   test_that("Learner can generate holdout set predictions", expect_equal(
     train_preds,
     holdout_preds
   ))
-  
+
   # test learner chaining
   chained_task <- fit_obj$chain()
   test_that("Chaining returns a task", expect_true(is(chained_task, "sl3_Task")))
@@ -49,22 +50,23 @@ test_learner <- function(learner, task, ...) {
   ))
 }
 
-test_learner(Lrnr_rpart, task)
-test_learner(Lrnr_rpart, task)
-
-## test xgboost learner:
+## test rpart learner:
 op <- options(sl3.verbose = TRUE)
-test_learner(Lrnr_xgboost, task, nrounds = 10) ## nrounds is always needed
-test_learner(Lrnr_xgboost, task, nrounds = 10, objective = "reg:linear") ## linear link function
-# test_learner(Lrnr_xgboost, task, nrounds = 50, objective = 'reg:logistic') ## logit-linear link function, need [0,1] outcomes
-test_learner(Lrnr_xgboost, task, nrounds = 50, booster = "gblinear") ## GLM, i.e., use linear model for at each boosting stage
-test_learner(Lrnr_xgboost, task, nrounds = 10, booster = "gbtree") ## GBM (default), i.e., use tree model for at each boosting stage
-# test_learner(Lrnr_xgboost, task, nrounds = 50, booster = 'dart') ## another type of tree model
-test_learner(Lrnr_xgboost, task_with_interactions, nrounds = 10, booster = "gbtree", covariates = c(
-  "cyl",
-  "disp", "drat", "cyl_disp", "hp_drat"
-))
-options(sl3.verbose = FALSE)
-test_learner(Lrnr_xgboost, task, nrounds = 50)
-
 options(op)
+test_learner(Lrnr_rpart, task)
+test_learner(Lrnr_rpart, task2)
+
+## instantiate Lrnr_rpart, train on task, and predict on task
+lrnr_rpart <- Lrnr_rpart$new()
+fit_lrnr_rpart <- lrnr_rpart$train(task)
+prd_lrnr_rpart <- fit_lrnr_rpart$predict()
+
+## fit rpart using the data from the task
+fit_rpart <- rpart(mpg ~ ., data = task$data)
+prd_rpart <- predict(fit_rpart)
+
+## test equivalence of prediction from Lrnr_rpart and rpart::rpart
+test_that("Lrnr_rpart predictions match those from rpart", {
+  expect_equal(prd_lrnr_rpart, prd_rpart)
+})
+
