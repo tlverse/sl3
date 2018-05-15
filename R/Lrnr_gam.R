97b794367fb0c35ee7a230e63e79d2c333651058
Nima Hejazi <nh@nimahejazi.org>
2018-05-14 22:42:52 -0700

    flesh out GAM learner and add test

--- R/Lrnr_gam.R
+++ R/Lrnr_gam.R
@@ -1,45 +1,48 @@
-#' GENERALIZED ADDITIVE MODELS
+#' Generalized Additive Models
 #'
-#' This learner uses \code{\link[gam]{gam.fit}} from \code{gam} to fit my favorite machine learning algorithm.
+#' This learner uses \code{\link[gam]{gam.fit}} from the \code{gam} package to
+#' efficiently fit generalized additive models.
 #'
 #' @docType class
-#' @importFrom R6 R6Class gam family predict
+#'
+#' @importFrom R6 R6Class
+#'
 #' @export
+#'
 #' @keywords data
-#' @return Learner object with methods for training and prediction. See \code{\link{Lrnr_base}} for documentation on learners.
+#'
+#' @return Learner object with methods for training and prediction. See
+#'  \code{\link{Lrnr_base}} for documentation on learners.
+#'
 #' @format \code{\link{R6Class}} object.
+#'
 #' @family Learners
 #'
 #' @section Parameters:
 #' \describe{
-#'   \item{\code{...}}{Parameters passed to \code{\link[gam]{gam}}.}
+#'   \item{\code{start}}{Starting values for the parameters in the additive
+#'     predictor.}
+#'   \item{\code{etastart}}{Starting values for the additive predictor.}
+#'   \item{\code{mustart}}{Starting values for the vector of means.}
+#'   \item{\code{...}}{Extra parameters passed to \code{\link[gam]{gam.fit}}.}
 #' }
-#'
-#'
-#' @template common_parameters
-Lrnr_gam <- R6Class(classname = "Lrnr_gam", inherit = Lrnr_base,
-                    portable = TRUE, class = TRUE,
-# Above, you should change Lrnr_template (in both the object name and the classname argument)
-# to a name that indicates what your learner does
+#
+Lrnr_gam <- R6Class(classname = "Lrnr_gam",
+                    inherit = Lrnr_base,
+                    portable = TRUE,
+                    class = TRUE,
   public = list(
-
-    # you can define default parameter values here
-    # if possible, your learner should define defaults for all required parameters
-    initialize = function(intercept = TRUE, ...) {
+    initialize = function(start = NULL,
+                          etastart = NULL,
+                          mustart = NULL,
+                          ...) {
       params <- args_to_list()
       super$initialize(params = params, ...)
     }
-
   ),
   private = list(
-    # list properties your learner supports here.
-    # Use sl3_list_properties() for a list of options
     .properties = c("continuous", "binomial", "weights", "offset"),
 
-    # list any packages required for your learner here.
-    .required_packages = c("gam"),
-
-    # .train takes task data and returns a fit object that can be used to generate predictions
     .train = function(task) {
       # generate an argument list from the parameters that were
       # captured when your learner was initialized.
@@ -47,47 +50,37 @@ Lrnr_gam <- R6Class(classname = "Lrnr_gam", inherit = Lrnr_base,
       args <- self$params
 
       # get outcome variable type
-      # prefering learner$params$outcome_type first, then task$outcome_type
+      # preferring learner$params$outcome_type first, then task$outcome_type
       outcome_type <- self$get_outcome_type(task)
-      # should pass something on to your learner indicating outcome_type
-      # e.g. family or objective
 
       # add task data to the argument list
       # what these arguments are called depends on the learner you are wrapping
-      args$x <- as.matrix(task$X_intercept)
-      args$y <- outcome_type$format(task$Y)
+      args$formula <- as.formula("task$Y ~ as.matrix(task$X)")
 
       # Sets family of distributon and appropriate linkage function
       # based on outcome_type if not specified by user
       if (is.null(args$family)) {
         args$family <- outcome_type$glm_family(return_object = TRUE)
       }
-      family_name <- args$family$family
-      linkinv_fun <- args$family$linkinv
-      link_fun <- args$family$linkfun
 
-      # only add arguments on weights and offset
-      # if those were specified when the task was generated
-      if(task$has_node("weights")){
+      if (task$has_node("weights")) {
         args$weights <- task$weights
       }
 
-      if(task$has_node("offset")){
+      if (task$has_node("offset")) {
         args$offset <- task$offset
       }
 
-      # call a function that fits your algorithm
-      # with the argument list you constructed
-      args$ctrl <- gam.control(trace = FALSE)
+      args$ctrl <- gam::gam.control(trace = FALSE)
+
+      # perform model fitting with gam::gam.fit
       SuppressGivenWarnings({
-        fit_object <- call_with_args(gam::gam.fit, args)
+        fit_object <- call_with_args(gam::gam, args)
       }, GetWarningsToSuppress())
 
-      # return the fit object, which will be stored
-      # in a learner object and returned from the call
-      # to learner$predict
-      # Lrnr_glm sets the following values to NULL,
-      # So we do the same for Lrnr_gam
+      # return the fit object, which will be stored in a learner object and
+      # returned from the call to learner$predict
+      # NOTE: Lrnr_glm sets the following to NULL -- doing the same for Lrnr_gam
       fit_object$additive.predictors <- NULL
       fit_object$weights <- NULL
       fit_object$prior.weights <- NULL
@@ -95,9 +88,6 @@ Lrnr_gam <- R6Class(classname = "Lrnr_gam", inherit = Lrnr_base,
       fit_object$residuals <- NULL
       fit_object$fitted.values <- NULL
       fit_object$effects <- NULL
-      fit_object$qr <- NULL
-      fit_object$linkinv_fun <- linkinv_fun
-      fit_object$link_fun <- link_fun
       fit_object$training_offset <- task$has_node("offset")
       return(fit_object)
     },
@@ -109,7 +99,8 @@ Lrnr_gam <- R6Class(classname = "Lrnr_gam", inherit = Lrnr_base,
       self$fit_object
 
       predictions <- predict(self$fit_object, task$X)
-      return(predictions)
-    }
+      return(as.numeric(predictions))
+    },
+    .required_packages = c("gam")
   )
 )
---
+++ tests/testthat/test-gam.R
@@ -0,0 +1,76 @@
+context("test-gam.R -- General testing for GAMs")
+
+library(sl3)
+library(testthat)
+library(dplyr)
+library(gam)
+
+# define test dataset
+data(mtcars)
+task <- sl3_Task$new(mtcars, covariates = c(
+  "cyl", "disp", "hp", "drat", "wt", "qsec",
+  "vs", "am", "gear", "carb"
+), outcome = "mpg")
+
+task2 <- sl3_Task$new(mtcars, covariates = c(
+  "cyl", "disp", "hp", "drat", "wt", "qsec",
+  "vs", "am", "gear", "carb"
+), outcome = "mpg")
+
+interactions <- list(c("cyl", "disp"), c("hp", "drat"))
+task_with_interactions <- task$add_interactions(interactions)
+task2 <- task2$add_interactions(interactions)
+
+test_learner <- function(learner, task, ...) {
+  # test learner definition this requires that a learner can be instantiated with
+  # only default arguments. Not sure if this is a reasonable requirement
+  learner_obj <- learner$new(...)
+  print(sprintf("Testing Learner: %s", learner_obj$name))
+  # test learner training
+  fit_obj <- learner_obj$train(task)
+  test_that("Learner can be trained on data", expect_true(fit_obj$is_trained))
+
+  # test learner prediction
+  train_preds <- fit_obj$predict()
+  test_that("Learner can generate training set predictions", expect_equal(
+    sl3:::safe_dim(train_preds)[1],
+    length(task$Y)
+  ))
+
+  holdout_preds <- fit_obj$predict(task2)
+  test_that("Learner can generate holdout set predictions", expect_equal(
+    train_preds,
+    holdout_preds
+  ))
+
+  # test learner chaining
+  chained_task <- fit_obj$chain()
+  test_that("Chaining returns a task", expect_true(is(chained_task, "sl3_Task")))
+  test_that("Chaining returns the correct number of rows", expect_equal(
+    nrow(chained_task$X),
+    nrow(task$X)
+  ))
+}
+
+## test GAM learner:
+op <- options(sl3.verbose = TRUE)
+options(op)
+test_learner(Lrnr_gam, task)
+test_learner(Lrnr_gam, task2)
+
+test_that("Lrnr_gam predictions match those from gam.fit", {
+  ## instantiate Lrnr_ranger, train on task, and predict on task
+  set.seed(73964)
+  lrnr_gam <- Lrnr_gam$new()
+  fit_lrnr_gam <- lrnr_gam$train(task)
+  prd_lrnr_gam <- fit_lrnr_gam$predict()
+
+  ## fit ranger using the data from the task
+  set.seed(73964)
+  fit_gam <- gam(formula = mpg ~ ., data = task$data)
+  prd_gam <- predict(fit_gam, data = task$data) %>%
+    as.numeric()
+
+  ## test equivalence of prediction from Lrnr_ranger and ranger::ranger
+  expect_equal(prd_lrnr_gam, prd_gam)
+})
