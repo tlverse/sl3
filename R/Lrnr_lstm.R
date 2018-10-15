1764a6848eb33241335463a9b922cb7f2fbae319
Jeremy Coyle <jeremyrcoyle@gmail.com>
2018-10-15 11:02:55 -0700

    fix Lrnr_lstm bug

--- R/Lrnr_lstm.R
+++ R/Lrnr_lstm.R
@@ -104,7 +104,7 @@ Lrnr_lstm <- R6Class(
 
       # Create output as in glm
       predictions <- as.numeric(predictions)
-      predictions <- structure(predictions, names = seq_len(predictions))
+      predictions <- structure(predictions, names = seq_along(predictions))
 
       return(predictions)
     },
