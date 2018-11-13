4cb70612edf6140013ddd5bdbe2f23d061ae77c9
Nima Hejazi <nh@nimahejazi.org>
2018-11-12 22:04:47 -0800

    better density visualizations

--- tests/testthat/test-hal_condensier.R
+++ tests/testthat/test-hal_condensier.R
@@ -1,3 +1,4 @@
+library(data.table)
 library(tidyverse)
 library(condensier)
 library(hal9001)
@@ -22,7 +23,7 @@ data_in <- sim_data_set()
 sl_task <- sl3_Task$new(data_in, covariates = "W", outcome = "A")
 
 # set up learner with HAL density
-lrn <- Lrnr_condensier$new(nbins = 10, bin_method = "equal.len", pool = TRUE, 
+lrn <- Lrnr_condensier$new(nbins = 20, bin_method = "equal.len", pool = TRUE,
                            bin_estimator = Lrnr_hal9001_density$new())
 
 # train and predict from learner
@@ -30,15 +31,17 @@ trained_lrn <- lrn$train(sl_task)
 
 # make separate tasks with W = 0 and W = 1
 n_samp <- 5000
-A_supp <- seq(-6, 6, length = n_samp)
+A_supp <- seq(-3, 3, length = n_samp)
 W0 <- rep(0, n_samp)
 data_W0 <- as.data.table(cbind(A_supp, W0))
 setnames(data_W0, c("A", "W"))
 task_W0 <- sl3_Task$new(data_W0, covariates = "W", outcome = "A")
 
 predictions_W0 <- trained_lrn$predict(task_W0)
-hist_W0 <- predictions_W0 %>%
-  ggplot(aes(x = likelihood)) + geom_histogram()
+hist_W0 <- as.data.table(list(A = A_supp, predictions_W0)) %>%
+  ggplot(aes(x = A, y = likelihood)) +
+  geom_smooth(method = "loess", se = FALSE)
+  ggtitle("Conditional density p(A | W = 0)")
 
 
 W1 <- rep(1, n_samp)
@@ -47,7 +50,8 @@ setnames(data_W1, c("A", "W"))
 task_W1 <- sl3_Task$new(data_W1, covariates = "W", outcome = "A")
 
 predictions_W1 <- trained_lrn$predict(task_W1)
-hist_W1 <- predictions_W1 %>%
-  ggplot(aes(x = likelihood)) + geom_histogram()
-ggsave(hist_W1, filename = "~/hal_condensier_example.pdf")
+hist_W1 <- as.data.table(list(A = A_supp, predictions_W1)) %>%
+  ggplot(aes(x = A, y = likelihood)) +
+  geom_smooth(method = "loess", se = FALSE) +
+  ggtitle("Conditional density p(A | W = 1)")
 
