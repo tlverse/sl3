8d737b0bb35ffa11877764ac00e87a450bb7f8d0
rachaelvphillips <rachaelvphillips@gmail.com>
2019-03-18 18:05:52 -0700

    variable importance function

---
+++ R/varimp.R
@@ -0,0 +1,56 @@
+#' Variable Importance Measure
+#'
+#' Function that takes a learner fitted task and loss function to generate a
+#' table where each row corresponds to the risk when we omit a covariate minus
+#' the risk when we do not omit the covariate. Specifically, we calculate the
+#' risk difference between the learner fit with a scrambled covariate and the
+#' leaner fit with the true covariate, across all covariates.
+#'
+#' @param fit A learner fitted task
+#' @param loss A loss function (see loss_functions.R)
+#'
+#' @return A table of risk differences for each covariate, where a higher risk
+#'         differnece indicates a more important covariate in predicting Y
+#'
+#' @name varimp
+#'
+#' @export
+#' @keywords variable importance
+
+varimp <- function(fit, loss){
+
+  task <- fit$training_task
+  dat <- task$data
+  X <- task$X
+  Y <- task$X
+
+  preds <- fit$predict()
+  risk <- mean(loss(Y, preds))
+
+  risk_diffs <- lapply(X, function(i){
+    # scramble cov column and give it the same name as the raw cov col
+    scrambled_col <- data.table(sample(unlist(dat[,i, with = FALSE]),
+                                       nrow(dat)))
+    names(scrambled_col) <- i
+
+    # replace raw col with scrambled col in the task
+    scrambled_col_names <- task$add_columns(scrambled_col)
+    scrambled_col_task <- task$next_in_chain(column_names = scrambled_col_names)
+
+    # obtain preds on the scrambled col task
+    scrambled_sl_preds <- fit$predict_fold(scrambled_col_task)
+
+    # risk on scrambled col task
+    risk_scrambled <- mean(loss(Y, scrambled_sl_preds))
+
+    # calculate risk difference
+    rd <- risk_scrambled - risk
+    return(rd)
+  })
+
+  names(risk_diffs) <- X
+  results <- data.table(X = rep(names(risk_diffs), sapply(risk_diffs,length)),
+                        risk_diff = unlist(risk_diffs))
+  results_ord <- results[ order(-risk_diff) ]
+  return(results_ord)
+}
