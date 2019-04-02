17a167f80679ab52027ec22181b81b4bb1bd63f7
Rachael Phillips <rachaelvphillips@users.noreply.github.com>
2019-04-01 23:00:24 -0500

    Added a variable importance function (#197)
    * variable importance function
    
    * bug fixes, add sample varimp, add test
    
    * fix namespace

---
+++ R/varimp.R
@@ -0,0 +1,63 @@
+#' Variable Importance Measure
+#'
+#' Function that takes a learner fitted task and loss function to generate a
+#' table where each row corresponds to the risk when we omit a covariate minus
+#' the risk when we do not omit the covariate. Specifically, we calculate the
+#' risk difference between the learner fit with a scrambled covariate and the
+#' leaner fit with the true covariate, across all covariates.
+#'
+#' @param fit A learner fitted task
+#' @param loss A loss function (see loss_functions.R)
+#' @param type either "permute" (for permutation based variable importance),
+#'             or "sample" (for sample based variable importance)
+#' @return A table of risk differences for each covariate, where a higher risk
+#'         difference indicates a more important covariate in predicting Y
+#'
+#' @name varimp
+#'
+#' @export
+#' @keywords variable importance
+varimp <- function(fit, loss, type=c("permute", "sample")){
+
+  task <- fit$training_task
+  dat <- task$data
+  X <- task$X
+  Y <- task$Y
+
+  preds <- fit$predict()
+  risk <- mean(loss(Y, preds))
+  type <- match.arg(type)
+  risk_diffs <- lapply(task$nodes$covariates, function(i){
+    if(type=="permute"){
+      # scramble cov column and give it the same name as the raw cov col
+      scrambled_col <- data.table(sample(unlist(dat[,i, with = FALSE]),
+                                       nrow(dat)))
+    } else {
+      col_to_scramble <- as.numeric(unlist(dat[,i, with = FALSE]))
+      scrambled_col <- data.table(runif(nrow(dat),
+                                        min = min(col_to_scramble),
+                                        max = max(col_to_scramble)))
+    }
+    names(scrambled_col) <- i
+
+    # replace raw col with scrambled col in the task
+    scrambled_col_names <- task$add_columns(scrambled_col)
+    scrambled_col_task <- task$next_in_chain(column_names = scrambled_col_names)
+
+    # obtain preds on the scrambled col task
+    scrambled_sl_preds <- fit$predict_fold(scrambled_col_task)
+
+    # risk on scrambled col task
+    risk_scrambled <- mean(loss(Y, scrambled_sl_preds))
+
+    # calculate risk difference
+    rd <- risk_scrambled - risk
+    return(rd)
+  })
+
+  names(risk_diffs) <- task$nodes$covariates
+  results <- data.table(X = names(risk_diffs),
+                        risk_diff = unlist(risk_diffs))
+  results_ord <- results[ order(-risk_diff) ]
+  return(results_ord)
+}
