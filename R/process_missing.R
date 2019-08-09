a64a7139369e637dbbda7c4d4d547a6e7c1ab039
Jeremy Coyle <jeremyrcoyle@gmail.com>
2019-08-09 09:16:41 -0700

    fix mode imputation

--- R/process_missing.R
+++ R/process_missing.R
@@ -10,6 +10,7 @@ impute_mode <- function(x) {
   count_df <- aggregate(count ~ x, data = data.frame(count = 1, x = x), sum)
   value <- count_df$x[which.max(count_df$count)]
   x[is.na(x)] <- value
+  return(x)
 }
 
 impute_by_type <- function(x) {
