7acedaa10603efdc6631c7b840dba1d7409969f3
Ivana <imalenica.stat@gmail.com>
2020-03-03 16:36:42 -0800

    Skip if no keras module detected

--- tests/testthat/test-keras.R
+++ tests/testthat/test-keras.R
@@ -13,38 +13,43 @@ if (FALSE) {
 }
 
 library(reticulate)
+library(testthat)
 set.seed(1)
-
-skip_if_no_foo <- function() {
-  have_foo <- reticulate::py_module_available("keras.models")
-
-  if (!have_foo) {
-    skip("keras.models not available for testing")
-  } else {
-    reticulate::import("keras.models")
-  }
-}
 
 trend_all <- 11:130 + rnorm(120, sd = 2)
 trend_all <- data.frame(data = trend_all)
 task <- sl3_Task$new(trend_all, covariates = "data", outcome = "data")
 
 test_that("Lrnr_lstm does what we expect", {
-  skip_if_no_foo()
-  lstm_learner <- Lrnr_lstm$new(epochs = 1)
-  lstm_fit <- lstm_learner$train(task)
-  lstm_preds <- lstm_fit$predict(task)
-
-  # expect_true(sum(lstm_preds)-28.95605 < 10^(-1))
-  expect_equal(length(lstm_preds), nrow(task$X) - 5)
+  have_foo <- reticulate::py_module_available("keras.models")
+  
+  if (!(have_foo)) {
+    skip("keras.models not available for testing")
+  } else {
+    reticulate::import("keras.models")
+    
+    lstm_learner <- Lrnr_lstm$new(epochs = 1)
+    lstm_fit <- lstm_learner$train(task)
+    lstm_preds <- lstm_fit$predict(task)
+    
+    expect_true(sum(lstm_preds)-28.95605 < 10^(-1))
+    #expect_equal(length(lstm_preds), nrow(task$X) - 5)
+  }
 })
 
-# test_that("Lrnr_bilstm does what we expect", {
-# skip_if_no_foo()
-# bilstm_learner <- Lrnr_bilstm$new(epochs = 1)
-# bilstm_fit <- bilstm_learner$train(task)
-# bilstm_preds <- bilstm_fit$predict(task)
-
-# expect_true(sum(bilstm_preds)-118.5766 < 10^(-1))
-# expect_equal(length(bilstm_preds), nrow(task$X) - 5)
-# })
+test_that("Lrnr_bilstm does what we expect", {
+  have_foo <- reticulate::py_module_available("keras.models")
+  
+  if (!(have_foo)) {
+    skip("keras.models not available for testing")
+  } else {
+    reticulate::import("keras.models")
+    
+    bilstm_learner <- Lrnr_bilstm$new(epochs = 1)
+    bilstm_fit <- bilstm_learner$train(task)
+    bilstm_preds <- bilstm_fit$predict(task)
+    
+    expect_true(sum(bilstm_preds)-118.5766 < 10^(-1))
+    #expect_equal(length(bilstm_preds), nrow(task$X) - 5)
+  }
+})
