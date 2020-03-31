3db3d6fd21c79f82478d68d5a7edf66914db309b
Ivana <imalenica.stat@gmail.com>
2020-03-31 02:15:25 -0700

    Missing )

--- tests/testthat/test-arima.R
+++ tests/testthat/test-arima.R
@@ -36,7 +36,7 @@ test_that("Lrnr_arima gives expected values with auto.arima", {
   arima_preds_2 <- as.numeric(arima_preds_2$pred)
   arima_preds_2 <- structure(arima_preds_2, names = 1)
 
-  expect_true(sum(arima_preds - arima_preds_2) < 10
+  expect_true(sum(arima_preds - arima_preds_2) < 10)
   expect_true(all.equal(arima_preds_2, arima_preds))
 })
 
@@ -50,7 +50,7 @@ test_that("Lrnr_arima gives expected values with arima order set", {
   arima_preds_2 <- as.numeric(arima_preds_2$pred)
   arima_preds_2 <- structure(arima_preds_2, names = 1)
 
-  expect_true(sum(arima_preds - arima_preds_2) < 10
+  expect_true(sum(arima_preds - arima_preds_2) < 10)
   expect_true(all.equal(arima_preds_2, arima_preds))
 })
 
