1248efc4dc927b9b00b34fb18c156295ca5daa6b
Rachael Phillips <rachael.phillips@accenture.com>
2020-06-17 16:11:11 -0700

    add cutoff based on no. covars

--- R/Lrnr_screener_coefs.R
+++ R/Lrnr_screener_coefs.R
@@ -22,14 +22,20 @@
 #' \describe{
 #'   \item{\code{learner}}{An instantiated learner to use for estimating
 #'     coefficients used in screening.}
-#'   \item{\code{threshold = 1e-3}}{Minimum size of coefficients to be kept.}
+#'   \item{\code{threshold = 1e-3}}{Minimum size of coefficients to be kept. 
+#'   If NULL, will select max_retain variables.}
+#'   \item{\code{min_retain = 2}}{Minimum number of variables to be kept.}
+#'   \item{\code{max_retain = NULL}}{Maximum number of variables to be kept. 
+#'   Default selects all with absolute coefficient above threshold.}
+#'   \item{\code{verbose = FALSE}}{Print selected variables.}
 #'   \item{\code{...}}{Other parameters passed to \code{learner}.}
 #' }
 Lrnr_screener_coefs <- R6Class(
   classname = "Lrnr_screener_coefs",
   inherit = Lrnr_base, portable = TRUE, class = TRUE,
   public = list(
-    initialize = function(learner, threshold = 1e-3, ...) {
+    initialize = function(learner, threshold = 1e-3, min_retain = 2, 
+                          max_retain = NULL, verbose = FALSE, ...) {
       params <- args_to_list()
       super$initialize(params = params, ...)
     }
@@ -38,23 +44,76 @@ Lrnr_screener_coefs <- R6Class(
     .properties = c("screener"),
 
     .train = function(task) {
+      
+      # check arguments
+      threshold <- self$params$threshold
+      min_retain <- self$params$min_retain
+      max_retain <- self$params$max_retain
+      covs <- task$nodes$covariates
+      
+      if(is.null(threshold) & is.null(max_retain)){
+        stop("threshold, max_retain or both must be provided")
+      }
+      
+      if(is.null(min_retain)){
+        stop("min_retain must be provided. ")
+      }
+      
+      if(length(min_retain) >= length(covs)){
+        stop("modify min_retain to be less than the number of covariates")
+      }
+      
+      if(length(max_retain) > length(covs)){
+        max_retain <- NULL
+        warning("max_retain is greater than the number of covariates, modifying max_retain to NULL")
+      }
+      
       learner <- self$params$learner
       fit <- learner$train(task)
       coefs <- as.vector(coef(fit))
-      coef_names <- rownames(coef(fit))
+      coef_names <- fit$rownames(coef(fit))
       if(is.null(coef_names)){
         coef_names <- names(coef(fit))
       }
       
       if(is.null(coef_names)){
-        stop("could not extract names from fit coefficients")
+        stop("could not extract names from fit coefficients, consider different learner or screener")
       }
       
-      covs <- task$nodes$covariates
+      # remove intercept
+      covs_idx <- which(gsub("\\..*","",coef_names) %in% covs)
+      coef_names <- coef_names[covs_idx]
+      coefs <- coefs[covs_idx]
       
-      selected_coefs <- coef_names[which(abs(coefs) > self$params$threshold)]
-      selected_coefs <- unique(gsub("\\..*","",selected_coefs))
-      selected <- intersect(selected_coefs, covs)
+      # sort by absolute value of coefficients
+      sorted <- sort(abs(coefs), decreasing = TRUE, index.return = TRUE)
+      coef_names_sorted <- coef_names[sorted$ix] 
+      coefs_sorted <- sorted$x
+      covs_sorted <- intersect(unique(coef_names_sorted), covs)
+      
+      if(!is.null(threshold)){
+        selected_coefs <- coef_names[which(coefs > threshold)]
+        selected <- intersect(unique(selected_coefs), covs)
+        if(length(selected) < min_retain){
+          selected <- covs_sorted[1:min_retain]
+        }
+        if( !is.null(max_retain) & (length(selected) > max_retain) ){
+          selected <- selected[1:max_retain]
+        }
+      }
+      
+      if(is.null(threshold)){
+        selected <- covs_sorted[1:max_retain]
+      }
+      
+      if(length(selected) == length(covs)){
+        warning("all covariates selected by Lrnr_screener_coefs, consider decreasing max_retain and/or increasing threshold")
+      }
+      if(verbose){
+        print(paste0("Lrnr_screener_coefs selected the following variables: ", 
+                     selected))
+      }
+      
       fit_object <- list(selected = selected)
       return(fit_object)
     },
--- tests/testthat/test-screeners.R
+++ tests/testthat/test-screeners.R
@@ -20,13 +20,35 @@ task <- sl3_Task$new(data.table::copy(cpp_imputed),
 lrnr_glmnet <- make_learner(Lrnr_glmnet)
 lrnr_glm <- make_learner(Lrnr_glm)
 lrnr_mean <- make_learner(Lrnr_mean)
+lrnr_bayesglm <- Lrnr_pkg_SuperLearner$new("SL.bayesglm")
 lrnrs <- make_learner(Stack, lrnr_glm, lrnr_mean)
 
-# Glmnet coef screener
+# Coefficient screener
 glmnet_screener <- make_learner(Lrnr_screener_coefs, lrnr_glmnet)
 debug_train(glmnet_screener)
 glmnet_screener_fit <- glmnet_screener$train(task)
 glmnet_screener_fit$fit_object$selected
+
+test_that("argument checks throw warnings/errors", {
+  expect_error(glmnet_screener$train(task, threshold = NULL, max_retain = NULL), 
+               "threshold, max_retain or both must be provided")
+  expect_error(glmnet_screener$train(task, min_retain = NULL), 
+               "min_retain must be provided")
+  expect_error(glmnet_screener$train(task, min_retain = 7), 
+               "modify min_retain to be less than the number of covariates")
+  expect_warning(glmnet_screener$train(task, min_retain = 9),
+                 "max_retain is greater than the number of covariates, modifying max_retain to NULL")
+})
+
+glm_screener <- make_learner(Lrnr_screener_coefs, lrnr_glm)
+debug_train(glm_screener)
+glm_screener_fit <- glm_screener$train(task)
+glm_screener_fit$fit_object$selected
+
+# bayesglm_screener <- make_learner(Lrnr_screener_coefs, lrnr_bayesglm)
+# debug_train(bayesglm_screener)
+# bayesglm_screener_fit <- bayesglm_screener$train(task)
+# bayesglm_screener_fit$fit_object$selected
 
 # Correlation P-value Screener
 screen_corP <- make_learner(Lrnr_screener_corP)
