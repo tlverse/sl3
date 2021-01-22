d77808df3f351851b1b5aba193ed5687b2f90869
rachaelvphillips <rachaelvphillips@gmail.com>
2021-01-22 16:29:13 -0700

    revamp sl3 importance

---
+++ R/importance.R
@@ -0,0 +1,163 @@
+#' Variable Importance
+#'
+#' Function that takes a cross-validated fit (i.e., cross-validated learner that
+#' has already been trained on a task), which could be a cross-validated single
+#' learner or super learner, to generate a loss-based variable importance
+#' measure for each predictor, where the predictors are the covariates in the
+#' trained task. This function generates a \code{data.table} in which each row
+#' corresponds to the risk difference or risk ratio between the following
+#' two risks: the risk when a predictor is permuted or removed, and the original
+#' risk (i.e., when all predictors are included). A higher risk ratio/difference
+#' corresponds to a more important predictor. A plot can be generated from the
+#' returned \code{data.table} by calling companion function
+#' \code{plot_importance}.
+#'
+#' @param fit A trained cross-validated learner (e.g., cv stack, super learner),
+#'  from which cross-validated predictions can be generated.
+#' @param loss The loss function for evaluating the risk. Defaults according to
+#'  outcome type: squared error loss for continuous outcomes, and negative
+#'  log-likelihood loss for discrete outcomes. See \code{\link{loss_functions}}.
+#' @param fold_number The fold number to use for obtaining the predictions
+#'  from the fit. Either a positive integer for obtaining predictions from a
+#'  specific fold's fit; \code{"full"} for obtaining predictions from a fit on
+#'  all of the data, or \code{"validation"} (default) for obtaining
+#'  cross-validated predictions, where the data used for training and prediction
+#'  never overlaps across the folds. Note that if a positive integer or
+#'  \code{"full"} is supplied, then there will be overlap between the  data
+#'  used for training and prediction.
+#' @param type Which method should be used to obscure the relationship between
+#'  the covariate and the outcome. When type is \code{"remove"} (default), each
+#'  covariate is removed from the task and the cross-validated learner is refit
+#'  to this modified task and then predictions are obtained from this refit.
+#'  When type is \code{"permute"}, each covariate is permuted (sampled without
+#'  replacement), and then predictions are obtained from this modified data with
+#'  one permuted covariate.
+#' @param importance_metric Either \code{"ratio"} (default), which returns
+#'  the risk with the permuted/removed X divided by observed risk with all X; or
+#'  \code{"difference"}, which returns the difference between the risk with the
+#'  permuted/removed X and the observed risk.
+#'
+#' @importFrom data.table data.table
+#'
+#' @return A \code{data.table} of variable importance for each covariate.
+#'
+#' @name importance
+#' @keywords variable importance
+#'
+#' @export
+importance <- function(fit, loss = NULL, fold_number = "validation",
+                       type = c("remove", "permute"),
+                       importance_metric = c("ratio", "difference")) {
+
+  # check arguments
+  if (!fit$is_trained) {
+    stop("Fit is not trained.")
+  }
+
+  # set defaults
+  importance_metric <- match.arg(importance_metric)
+  type <- match.arg(type)
+
+  # extract task and data
+  task <- fit$training_task
+  d <- task$data
+  X <- task$nodes$covariates
+  Y <- task$Y
+
+  if (is.null(loss)) {
+    outcome_type <- task$outcome_type$type
+    if (outcome_type %in% c("constant", "binomial")) {
+      loss <- loss_loglik_binomial
+    } else if (outcome_type == "categorical") {
+      loss <- loss_loglik_multinomial
+    } else if (outcome_type == "continuous") {
+      loss <- loss_squared_error
+    } else if (outcome_type == "multivariate") {
+      loss <- loss_squared_error_multivariate
+    } else {
+      stop(paste0(
+        "No default loss for outcome type ", outcome_type,
+        ". Please specify your own."
+      ))
+    }
+  }
+
+  # get predictions and risk
+  pred <- fit$predict_fold(task, fold_number = fold_number)
+  original_risk <- mean(loss(pred, Y))
+
+  # X-length list of importance scores
+  res_list <- lapply(X, function(x) {
+    if (type == "permute") {
+      # permute covariate x, and give it the same name as the original x
+      x_permuted <- data.table(sample(unlist(d[, x, with = FALSE]), nrow(d)))
+      names(x_permuted) <- x
+      # replace original x with permuted x, and update task with permuted x
+      x_permuted_name <- task$add_columns(x_permuted)
+      task_x_permuted <- task$next_in_chain(column_names = x_permuted_name)
+      # obtain predictions & risk on the new task with permuted x
+      x_permuted_pred <- fit$predict_fold(task_x_permuted, fold_number)
+      no_x_risk <- mean(loss(x_permuted_pred, Y))
+    } else if (type == "remove") {
+      # modify task to not include covariate x
+      x_removed_task <- task$next_in_chain(covariates = X[-which(X == x)])
+      x_removed_fit <- fit$train(x_removed_task)
+      x_removed_pred <- x_removed_fit$predict_fold(x_removed_task, fold_number)
+      no_x_risk <- mean(loss(x_removed_pred, Y))
+    }
+    # evaluate importance
+    if (importance_metric == "ratio") {
+      return(no_x_risk / original_risk)
+    } else if (importance_metric == "difference") {
+      return(no_x_risk - original_risk)
+    }
+  })
+  names(res_list) <- X
+
+  # importance results ordered by decreasing importance
+  if (importance_metric == "ratio") {
+    res <- data.table(X = names(res_list), risk_ratio = unlist(res_list))
+    return(res[order(-res$risk_ratio)])
+  } else if (importance_metric == "difference") {
+    res <- data.table(X = names(res_list), risk_difference = unlist(res_list))
+    return(res[order(-res$risk_difference)])
+  }
+}
+
+#' Variable Importance Plot
+#'
+#' @param x The 2-column \code{data.table} returned by the call to
+#'  \code{importance}, where the first column is the predictors and the second
+#'  col
+#' @param nvar The maximum number of predictors to be plotted. Defaults to 30.
+#' @param ... Other parameters passed to \code{\link[graphics]{dotchart}}.
+#'
+#' @return A \code{\link[graphics]{dotchart}} of variable importance.
+#'
+#' @importFrom graphics dotchart
+#'
+#' @name importance_plot
+#' @keywords variable importance
+#'
+#' @export
+importance_plot <- function(x, nvar = min(30, nrow(x)), ...) {
+
+  # get the importance metric
+  if (grepl("ratio", colnames(x)[2])) {
+    xlab <- "Risk Ratio"
+  } else if (grepl("difference", colnames(x)[2])) {
+    xlab <- "Risk Difference"
+  }
+
+  # sort by decreasing importance and then subset
+  x_sorted <- x[order(-x[, 2])]
+  x_plotting <- x_sorted[1:(min(nvar, nrow(x_sorted))), ]
+
+  # modify into named vector for dotchart
+  importance_scores <- x_plotting[[2]]
+  names(importance_scores) <- x_plotting[, "X", with = F]
+  dotchart(importance_scores,
+    xlab = xlab, ylab = "",
+    xlim = c(min(importance_scores), max(importance_scores)), ...
+  )
+}
--- tests/testthat/test-varimp.R
+++ tests/testthat/test-varimp.R
@@ -4,17 +4,15 @@ context("test_varimp.R -- Variable Importance")
 options(sl3.verbose = TRUE)
 library(sl3)
 library(origami)
-library(SuperLearner)
 library(data.table)
 data(cpp_imputed)
 setDT(cpp_imputed)
 cpp_imputed[, parity_cat := factor(ifelse(parity < 4, parity, 4))]
-covars <- c("apgar1", "apgar5", "parity_cat", "gagebrth", "mage", "meducyrs", "sexn")
+covars <- c("apgar1", "parity_cat", "sexn")
 outcome <- "haz"
 
-task <- sl3_Task$new(data.table::copy(cpp_imputed),
-  covariates = covars,
-  outcome = outcome,
+task <- sl3_Task$new(cpp_imputed,
+  covariates = covars, outcome = outcome,
   folds = origami::make_folds(cpp_imputed, V = 3)
 )
 
@@ -23,7 +21,27 @@ lrnr_mean <- make_learner(Lrnr_mean)
 lrnr_glm <- make_learner(Lrnr_glm)
 learners <- make_learner(Stack, lrnr_glmnet, lrnr_mean, lrnr_glm)
 sl <- make_learner(Lrnr_sl, learners)
+test_that("sl3 importance fails when fit isn't trained", {
+  expect_error(importance(sl))
+})
 fit <- sl$train(task)
 
-permute_imp_ratio <- varimp(fit, loss_squared_error)
-permute_imp_diff <- varimp(fit, loss_squared_error, type = "difference")
+test_that("Test various implementations of sl3 importance", {
+  remove_validation_risk_ratio <- importance(fit)
+  remove_full_risk_ratio <- importance(fit, fold_number = "full")
+  remove_validation_risk_diff <- importance(fit, importance_metric = "difference")
+  remove_full_risk_diff <- importance(fit,
+    fold_number = "full",
+    importance_metric = "difference"
+  )
+  permute_validation_risk_ratio <- importance(fit, type = "permute")
+  permute_full_risk_ratio <- importance(fit, type = "permute", fold_number = "full")
+  permute_validation_risk_diff <- importance(fit,
+    type = "permute",
+    importance_metric = "difference"
+  )
+  permute_full_risk_diff <- importance(fit,
+    type = "permute", fold_number = "full",
+    importance_metric = "difference"
+  )
+})
