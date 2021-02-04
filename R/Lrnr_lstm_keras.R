606a6fbd5ed42d73a2f2fc02b5c51acc96f9e3b3
Ivana <imalenica.stat@gmail.com>
2021-02-03 16:24:17 -0800

    return overwritten changes :/

--- R/Lrnr_lstm_keras.R
+++ R/Lrnr_lstm_keras.R
@@ -17,11 +17,12 @@
 #' @field units Positive integer, dimensionality of the output space.
 #' @field dropout Float between 0 and 1. Fraction of the input units to drop.
 #' @field recurrent_dropout Float between 0 and 1. Fraction of the units to drop for the linear transformation of the recurrent state.
-#' @field activation Activation function to use. If you pass NULL, no activation is applied (ie. "linear" activation: a(x) = x).
-#' @field recurrent_activation Activation function to use for the recurrent step.
+#' @field activation Activation function to use. If you pass NULL, no activation is applied (ie. "linear" activation: a(x) = x).                      
+#' @field recurrent_activation Activation function to use for the recurrent step. 
+#' @field recurrent_out Activation function to use for the output step. 
 #' @field epochs Number of epochs to train the model.
-#' @field lr Learning rate.
-#'
+#' @field lr Learning rate. 
+#' @field layers How many lstm layers. Only allows for 1 or 2.
 #'
 #' @importFrom assertthat assert_that is.count is.flag
 #'
@@ -37,14 +38,16 @@ Lrnr_lstm_keras <- R6Class(
                           recurrent_dropout = 0.2,
                           activation = "tanh",
                           recurrent_activation = "hard_sigmoid",
+                          activation_out = "linear",
                           epochs = 10,
                           lr = 0.001,
+                          layers = 1,
                           ...) {
       params <- list(
-        batch_size = batch_size, units = units, dropout = dropout,
-        recurrent_dropout = recurrent_dropout, activation = activation,
-        recurrent_activation = recurrent_activation, epochs = epochs,
-        lr = lr, ...
+        batch_size=batch_size, units=units, dropout=dropout,
+        recurrent_dropout=recurrent_dropout, activation=activation,
+        recurrent_activation=recurrent_activation, epochs=epochs,
+        lr=lr, layers = layers, ...
       )
       super$initialize(params = params, ...)
     }
@@ -53,77 +56,87 @@ Lrnr_lstm_keras <- R6Class(
     .properties = c("timeseries", "continuous"),
     .train = function(task) {
       args <- self$params
-
-      # Get data
+      
+      #Get data
       data <- task$data
       X <- task$X
       Y <- task$Y
-
-      args$x <- array(
-        data = as.matrix(X),
-        dim = c(nrow(data), 1, ncol(X))
-      )
-      args$y <- array(
-        data = task$Y,
-        dim = c(nrow(data), 1)
-      )
-
-      fit_object <- keras_model_sequential() %>%
-        layer_lstm(
-          units = args$units,
-          activation = args$activation,
-          recurrent_activation = args$recurrent_activation,
-          dropout = args$dropout,
-          recurrent_dropout = args$recurrent_dropout,
-          input_shape = c(1, ncol(X))
-        ) %>%
-        layer_dense(units = 1)
-
-      # TO DO: allow for losses to be passed as well
-      if (task$outcome_type$type == "continuous") {
+      
+      args$x = array(data = as.matrix(X), 
+                     dim = c(nrow(data), 1, ncol(X)))
+      args$y = array(data = task$Y, 
+                     dim = c(nrow(data), 1))
+      
+      if(args$layers == 1){
+        fit_object <- keras_model_sequential() %>% 
+          layer_lstm(units = args$units,
+                     activation = args$activation,
+                     recurrent_activation = args$recurrent_activation,
+                     dropout = args$dropout,
+                     recurrent_dropout = args$recurrent_dropout,
+                     input_shape = c(1, ncol(X))) %>%
+          layer_dense(units = 1,
+                      activation = args$activation_out)
+      }else{
+        fit_object <- keras_model_sequential() %>% 
+          layer_lstm(units = args$units,
+                     activation = args$activation,
+                     recurrent_activation = args$recurrent_activation,
+                     dropout = args$dropout,
+                     recurrent_dropout = args$recurrent_dropout,
+                     input_shape = c(1, ncol(X)),
+                     return_sequences=TRUE) %>%
+          #layer_dropout(rate = 0.5) %>%
+          layer_lstm(units = args$units,
+                     activation = args$activation,
+                     recurrent_activation = args$recurrent_activation,
+                     dropout = args$dropout,
+                     recurrent_dropout = args$recurrent_dropout,
+                     return_sequences = FALSE) %>%
+          layer_dense(units = 1, 
+                      activation = args$activation_out)
+      }
+      
+      #TO DO: allow for losses to be passed as well
+      if(task$outcome_type$type=="continuous"){
         loss <- "mse"
-      } else if (task$outcome_type$type == "binary") {
+      }else if(task$outcome_type$type=="binary"){
         loss <- "binary_crossentropy"
       }
-
+      
       fit_object %>% compile(
         optimizer = optimizer_rmsprop(lr = args$lr),
         loss = loss
       )
-
-      fit_object %>% fit(
-        x = args$x,
-        y = args$y,
-        batch_size = args$batch_size,
-        epochs = args$epochs,
-        shuffle = FALSE
-      )
-
+      
+      fit_object %>% fit(x = args$x,
+                         y = args$y,
+                         batch_size = args$batch_size,
+                         epochs = args$epochs,
+                         shuffle = FALSE)
+      
       return(fit_object)
     },
-
+    
     .predict = function(task = NULL) {
       args <- self$params
-
-      # Get data
+      
+      #Get data
       data <- task$data
       X <- task$X
-
-      args$x <- array(
-        data = as.matrix(X),
-        dim = c(nrow(data), 1, ncol(X))
-      )
-
+      
+      args$x = array(data = as.matrix(X), 
+                     dim = c(nrow(data), 1, ncol(X)))
+      
       model <- private$.fit_object
-
+      
       predictions <- model %>% predict(args$x,
-        batch_size = args$batch_size
-      )
-
+                                       batch_size = args$batch_size)
+      
       # Create output as in glm
       predictions <- as.numeric(predictions)
       predictions <- structure(predictions, names = seq_along(predictions))
-
+      
       return(predictions)
     },
     .required_packages = c("keras")
