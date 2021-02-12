dcfdf24cb9d72d6b3fd6bad8beda40bed1b49f83
carpenitoThomas <carpenito.t@gmail.com>
2021-02-12 17:34:12 -0500

    Adds additional testing for bartMachine and dbarts

--- tests/testthat/test-dbarts.R
+++ tests/testthat/test-dbarts.R
@@ -39,6 +39,26 @@ outcome <- names(y)
 data <- cbind.data.frame(y, x)
 task <- sl3_Task$new(data, covariates = covars, outcome = outcome)
 
+test_that("Lrnr_dbarts produces results matching those of dbarts::barts", {
+  # get predictions from Lrnr_* wrapper
+  set.seed(123)
+  lrnr_dbarts <- make_learner(Lrnr_dbarts)
+  fit <- lrnr_dbarts$train(task)
+  preds <- fit$predict(task)
+  
+  # get predictions from classic implementation
+  set.seed(123)
+  fit_classic <- dbarts::bart(
+    x.train = data.frame(task$X), y.train = task$Y, keeptrees = TRUE, ndpost = 500
+  )
+  
+  preds_classic <- rowMeans(t(predict(fit_classic, newdata = task$X)))
+  
+  # check equality of predictions
+  expect_equal(preds, as.numeric(preds_classic))
+})
+
+
 test_that("Lrnr_dbarts with continuous outcome works", {
   dbart_learner <- Lrnr_dbarts$new(ndpost = 200)
   dbart_fit <- dbart_learner$train(task)
