0b5154bdeda119e8a7e85dcf1c9ed834006c8041
Nima Hejazi <nh@nimahejazi.org>
2021-02-22 12:35:13 -0800

    better Lrnr_ranger docs as CRAN example

--- R/Lrnr_ranger.R
+++ R/Lrnr_ranger.R
@@ -1,35 +1,52 @@
-#' Ranger - A Fast Implementation of Random Forests
+#' Ranger: A Faster Implementation of Random Forests
 #'
 #' This learner provides fitting procedures for a fast implementation of Random
-#' Forests, particularly suited for high dimensional data, using the
-#' \code{ranger} package, using the function \code{\link[ranger]{ranger}}.
+#' Forests, particularly suited for high dimensional data, using the routines
+#' from \pkg{ranger} thru a call to the function \code{\link[ranger]{ranger}}.
+#' Variable importance functionality is also provided through invocation of the
+#' \code{\link[ranger]{importance}} method.
 #'
 #' @docType class
 #'
 #' @importFrom R6 R6Class
+#' @importFrom stats predict
+#' @importFrom ranger ranger importance
 #'
 #' @export
 #'
 #' @keywords data
 #'
-#' @return Learner object with methods for training and prediction. See
-#'  \code{\link{Lrnr_base}} for documentation on learners.
+#' @return A learner object inheriting from \code{\link{Lrnr_base}} with
+#'  methods for training and prediction. For a full list of learner
+#'  functionality, see the complete documentation of \code{\link{Lrnr_base}}.
 #'
-#' @format \code{\link{R6Class}} object.
+#' @format An \code{\link[R6]{R6Class}} object inheriting from
+#'  \code{\link{Lrnr_base}}.
 #'
 #' @family Learners
 #'
 #' @section Parameters:
-#' \describe{
-#'   \item{\code{num.trees = 500}}{Number of trees to be used in growing the
-#'     forest.}
-#'   \item{\code{write.forest = TRUE}}{If \code{TRUE}, forest is stored, which
-#'     is required for prediction. Set to \code{FALSE} to reduce memory usage if
-#'     no prediction is intended.}
-#'   \item{\code{...}}{Other parameters passed to \code{\link[ranger]{ranger}}.
-#'     See its documentation for details.}
-#' }
-#
+#'  - \code{num.trees = 500}: Number of trees to be used in growing the forest.
+#'  - \code{write.forest = TRUE}: If \code{TRUE}, forest is stored, which is
+#'      required for prediction. Set to \code{FALSE} to reduce memory usage if
+#'      downstream prediction is not intended.
+#'  - \code{...}: Other parameters passed to \code{\link[ranger]{ranger}}. See
+#'      its documentation for details.
+#'
+#' @examples
+#' data(mtcars)
+#' mtcars_task <- sl3_Task$new(
+#'   data = mtcars,
+#'   covariates = c("cyl", "disp", "hp", "drat", "wt", "qsec", "vs", "am",
+#'                  "gear", "carb"),
+#'   outcome = "mpg"
+#' )
+#' # simple prediction
+#' ranger_learner <- Lrnr_ranger$new()
+#' ranger_fit <- ranger_learner$train(mtcars_task)
+#' ranger_preds <- ranger_fit$predict()
+#'
+#' # TODO: variable importance
 Lrnr_ranger <- R6Class(
   classname = "Lrnr_ranger", inherit = Lrnr_base,
   portable = TRUE, class = TRUE,
@@ -44,8 +61,8 @@ Lrnr_ranger <- R6Class(
     importance = function(...) {
       if (self$fit_object$importance.mode == "none") {
         stop(
-          "This learner was instantiated with default argument, ",
-          "importance=none. Modify this argument to measure importance."
+          "This learner was instantiated with the default argument, ",
+          "importance=none. Modify this to assess variable importance."
         )
       }
       self$assert_trained()
@@ -55,7 +72,8 @@ Lrnr_ranger <- R6Class(
       args$x <- self$fit_object
 
       # calculate importance metrics
-      importance_result <- call_with_args(ranger::importance, args, keep_all = T)
+      importance_result <- call_with_args(ranger::importance, args,
+                                          keep_all = TRUE)
 
       # sort according to decreasing importance
       return(importance_result[order(importance_result, decreasing = TRUE)])
@@ -63,7 +81,8 @@ Lrnr_ranger <- R6Class(
   ),
 
   private = list(
-    .properties = c("continuous", "binomial", "categorical", "importance", "weights"),
+    .properties = c("continuous", "binomial", "categorical", "importance",
+                    "weights"),
 
     .train = function(task) {
       args <- self$params
@@ -80,7 +99,6 @@ Lrnr_ranger <- R6Class(
     },
 
     .predict = function(task) {
-
       # extract numeric predictions from custom class ranger.prediction
       predictions <- stats::predict(
         private$.fit_object,
