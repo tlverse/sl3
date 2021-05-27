7c0477d8e9693b036298d48ebb2efe190ba327c0
rachaelvp <rachaelvphillips@gmail.com>
2021-05-26 21:01:58 -0700

    formula argument

--- R/Lrnr_glm.R
+++ R/Lrnr_glm.R
@@ -23,6 +23,15 @@
 #'
 #' @section Parameters:
 #'   - \code{intercept = TRUE}: Should an intercept be included in the model?
+#'     This argument is ignored when \code{formula} argument is not \code{NULL}.
+#'   - \code{formula = NULL}: An object of class \code{formula} containing a
+#'     description for the \code{\link[stats]{glm}} model to fit. NOTE that the
+#'     \code{formula}'s response variable (i.e., variable before "\code{~}")
+#'     must be identical to the outcome name as it's provided in the
+#'     \code{task} and the \code{formula}'s regressors (i.e., variables after
+#'     "\code{~}") must correspond to the column names in \code{X}, where
+#'     \code{X} is the \code{task}'s processed dataset that's used for training
+#'     learners and it can be accessed via \code{task$X}.
 #'   - \code{...}: Other parameters passed to \code{\link[stats]{glm}} or
 #'       \code{\link[stats]{glm.fit}}.
 #'
@@ -48,7 +57,7 @@ Lrnr_glm <- R6Class(
   classname = "Lrnr_glm",
   inherit = Lrnr_base, portable = TRUE, class = TRUE,
   public = list(
-    initialize = function(intercept = TRUE, ...) {
+    initialize = function(intercept = TRUE, formula = NULL, ...) {
       super$initialize(params = args_to_list(), ...)
     }
   ),
@@ -66,12 +75,31 @@ Lrnr_glm <- R6Class(
       link_fun <- args$family$linkfun
 
       # specify data
-      if (args$intercept) {
-        args$x <- as.matrix(task$X_intercept)
+      if (!is.null(args$formula)) {
+        if (class(args$formula) != "formula") {
+          print("Converting the provided formula to formula object")
+          args$formula <- as.formula(args$formula)
+        }
+        if (!all.vars(args$formula)[1] == task$nodes$outcome) {
+          stop(paste0(
+            "Formula outcome name ", all.vars(args$formula)[1],
+            " does not match the task's outcome name ", task$nodes$outcome
+          ))
+        }
+        if (!all(all.vars(args$formula)[-1] %in% colnames(task$X))) {
+          stop("Regressor variables in the formula are not columns in task$X")
+        }
+        args$data <- data.frame(outcome_type$format(task$Y), task$X)
+        colnames(args$data)[1] <- task$nodes$outcome
       } else {
-        args$x <- as.matrix(task$X)
+        if (args$intercept) {
+          args$x <- as.matrix(task$X_intercept)
+        } else {
+          args$x <- as.matrix(task$X)
+        }
+        args$y <- outcome_type$format(task$Y)
       }
-      args$y <- outcome_type$format(task$Y)
+
 
       if (task$has_node("weights")) {
         args$weights <- task$weights
@@ -82,12 +110,22 @@ Lrnr_glm <- R6Class(
       }
 
       args$control <- glm.control(trace = FALSE)
-      SuppressGivenWarnings(
-        {
-          fit_object <- call_with_args(stats::glm.fit, args)
-        },
-        GetWarningsToSuppress()
-      )
+
+      if (!is.null(args$formula)) {
+        SuppressGivenWarnings(
+          {
+            fit_object <- call_with_args(stats::glm, args)
+          },
+          GetWarningsToSuppress()
+        )
+      } else {
+        SuppressGivenWarnings(
+          {
+            fit_object <- call_with_args(stats::glm.fit, args)
+          },
+          GetWarningsToSuppress()
+        )
+      }
 
       fit_object$linear.predictors <- NULL
       fit_object$weights <- NULL
@@ -96,7 +134,7 @@ Lrnr_glm <- R6Class(
       fit_object$residuals <- NULL
       fit_object$fitted.values <- NULL
       fit_object$effects <- NULL
-      fit_object$qr <- NULL
+      fit_object$qr$qr <- NULL
       fit_object$linkinv_fun <- linkinv_fun
       fit_object$link_fun <- link_fun
       fit_object$training_offset <- task$has_node("offset")
@@ -104,29 +142,38 @@ Lrnr_glm <- R6Class(
     },
     .predict = function(task) {
       verbose <- getOption("sl3.verbose")
-      if (self$params$intercept) {
+
+      if (self$params$intercept & is.null(self$params$formula)) {
         X <- task$X_intercept
       } else {
         X <- task$X
       }
 
-      predictions <- rep.int(NA, nrow(X))
-      if (nrow(X) > 0) {
-        coef <- self$fit_object$coef
-        if (!all(is.na(coef))) {
-          eta <- as.matrix(X
-          [, which(!is.na(coef)),
-            drop = FALSE,
-            with = FALSE
-          ]) %*% coef[!is.na(coef)]
+      coef <- self$fit_object$coef
 
+      if (nrow(X) > 0 & !all(is.na(coef))) {
+        if (!is.null(self$params$formula)) {
+          predictions <- stats::predict(
+            private$.fit_object,
+            newdata = X, type = "response"
+          )
+          if (task$outcome_type$type == "categorical") {
+            predictions <- pack_predictions(predictions)
+          }
+        } else {
+          X <- as.matrix(X[, which(!is.na(coef)), drop = FALSE, with = FALSE])
+          eta <- X %*% coef[!is.na(coef)]
           if (self$fit_object$training_offset) {
-            offset <- task$offset_transformed(self$fit_object$link_fun, for_prediction = TRUE)
+            offset <- task$offset_transformed(
+              self$fit_object$link_fun,
+              for_prediction = TRUE
+            )
             eta <- eta + offset
           }
-
           predictions <- as.vector(self$fit_object$linkinv_fun(eta))
         }
+      } else {
+        predictions <- rep.int(NA, nrow(X))
       }
       return(predictions)
     }
