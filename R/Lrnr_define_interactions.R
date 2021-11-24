71d9fd2f8a02b30e92182d95400c21514bd94e7d
rachaelvp <rachaelvphillips@gmail.com>
2021-11-23 16:11:01 -0800

    clarify interaction arg

--- R/Lrnr_define_interactions.R
+++ R/Lrnr_define_interactions.R
@@ -1,30 +1,44 @@
 #' Define interactions terms
 #'
-#' This learner adds interactions to its chained task
+#' This learner adds interactions to its chained task. Intended for use in a
+#' Pipeline, defining a coupling of the interactions with the learner.
 #'
 #' @docType class
 #'
 #' @importFrom R6 R6Class
-#' @importFrom assertthat assert_that is.count is.flag
+#' @importFrom data.table data.table
 #'
 #' @export
 #'
 #' @keywords data
 #'
-#' @return Learner object with methods for training and prediction. See
-#'  \code{\link{Lrnr_base}} for documentation on learners.
+#' @return A learner object inheriting from \code{\link{Lrnr_base}} with
+#'  methods for training and prediction. For a full list of learner
+#'  functionality, see the complete documentation of \code{\link{Lrnr_base}}.
 #'
-#' @format \code{\link{R6Class}} object.
+#' @format An \code{\link[R6]{R6Class}} object inheriting from
+#'  \code{\link{Lrnr_base}}.
 #'
 #' @family Learners
 #'
 #' @section Parameters:
-#' \describe{
-#'   \item{\code{interactions}}{A list of lists, with each inner list containing
-#'     the covariates from which to create interaction terms.}
-#'   \item{\code{warn_on_existing}}{If \code{TRUE}, produce a warning if there
-#'     is already a column with a name matching this given interaction term.}
-#' }
+#'   - \code{interactions}: A list whose elements are a character
+#'     vector of covariates from which to create interaction terms.
+#'   - \code{warn_on_existing}: If \code{TRUE}, produce a warning if there
+#'     is already a column with a name matching this given interaction term.
+#'
+#' @examples
+#' data(cpp_imputed)
+#' covars <- c("apgar1", "apgar5", "parity", "gagebrth", "mage", "meducyrs", "sexn")
+#' outcome <- "haz"
+#' task <- sl3_Task$new(cpp_imputed, covariates = covars, outcome = outcome)
+#' interactions <- list(c("apgar1", "parity"), c("apgar5", "parity"))
+#' lrnr_interact <- Lrnr_define_interactions$new(
+#'   list(c("apgar1", "parity"), c("apgar5", "parity"))
+#' )
+#' lrnr_glm <- Lrnr_glm$new()
+#' interaction_pipeline_glm <- make_learner(Pipeline, lrnr_interact, lrnr_glm)
+#' fit <- interaction_pipeline_glm$train(task)
 Lrnr_define_interactions <- R6Class(
   classname = "Lrnr_define_interactions",
   inherit = Lrnr_base,
