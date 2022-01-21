dcc8cff5aa389413cacefe173bd27bf2d0be5dc6
rachaelvp <rachaelvphillips@gmail.com>
2022-01-20 16:29:01 -0800

    make site and style changes

--- tests/testthat/test-glmtree.R
+++ tests/testthat/test-glmtree.R
@@ -5,12 +5,11 @@ context("test-Lrnr_glmtree.R -- General testing for GLMtree")
 data(mtcars)
 
 test_that("Lrnr_glmtree continuous outcome preds match those from glmtree", {
-  
   task <- sl3_Task$new(mtcars, covariates = c(
     "cyl", "disp", "hp", "drat", "wt", "qsec",
     "vs", "am", "gear", "carb"
   ), outcome = "mpg")
-  
+
   ## instantiate Lrnr_glmtree, train on task, and predict on task
   lrnr_glmtree <- Lrnr_glmtree$new()
   fit_lrnr_glmtree <- lrnr_glmtree$train(task)
@@ -25,72 +24,71 @@ test_that("Lrnr_glmtree continuous outcome preds match those from glmtree", {
 })
 
 test_that("Lrnr_glmtree binary outcome preds match those from glmtree", {
-  
   task <- sl3_Task$new(mtcars, covariates = c(
     "cyl", "disp", "hp", "drat", "wt", "qsec",
     "mpg", "am", "gear", "carb"
   ), outcome = "vs")
-  
+
   ## instantiate Lrnr_glmtree, train on task, and predict on task
   lrnr_glmtree <- Lrnr_glmtree$new()
   fit_lrnr_glmtree <- lrnr_glmtree$train(task)
   prd_lrnr_glmtree <- fit_lrnr_glmtree$predict()
-  
+
   ## fit glmtree using the data from the task
   fit_glmtree <- glmtree(vs ~ ., data = task$data)
   prd_glmtree <- predict(fit_glmtree, newdata = task$data)
-  
+
   ## test equivalence of prediction from Lrnr_glmtree and glmtree::glmtree
   expect_equal(prd_lrnr_glmtree, as.numeric(prd_glmtree))
 })
 
 test_that("Lrnr_glmtree includes offset correctly", {
-  
-  task <- sl3_Task$new(mtcars, 
-                       covariates = c("disp", "hp",  "wt"), 
-                       outcome = "mpg",
-                       offset = "drat")
-  
+  task <- sl3_Task$new(mtcars,
+    covariates = c("disp", "hp", "wt"),
+    outcome = "mpg",
+    offset = "drat"
+  )
+
   ## instantiate Lrnr_glmtree, train on task, and predict on task
   lrnr_glmtree <- Lrnr_glmtree$new(alpha = 0.9, prune = "AIC")
   fit_lrnr_glmtree <- lrnr_glmtree$train(task)
   prd_lrnr_glmtree <- suppressWarnings(fit_lrnr_glmtree$predict())
-  
+
   # fit glmtree with same specification as sl3
-  # we need to set max_depth=10, since that was set as the default in 
+  # we need to set max_depth=10, since that was set as the default in
   # Lrnr_glmtree and it is different from the partykit::glmtree default of Inf
   offset <- task$data$drat
   d <- task$data
   fit_glmtree <- do.call(partykit::glmtree, list(
-    formula = as.formula(mpg ~ disp + hp + wt), 
-    offset = offset, data = d, 
+    formula = as.formula(mpg ~ disp + hp + wt),
+    offset = offset, data = d,
     alpha = 0.9, prune = "AIC", maxdepth = 10
   ))
   prd_glmtree <- suppressWarnings(predict(fit_glmtree, newdata = d))
-  
+
   ## test equivalence of prediction from Lrnr_glmtree and partykit::glmtree
   expect_equal(prd_lrnr_glmtree, as.numeric(prd_glmtree))
 })
 
 test_that("Lrnr_glmtree errors when covariates in formula are misspecified", {
-  
-  task <- sl3_Task$new(mtcars, 
-                       covariates = c("disp", "hp",  "wt"), 
-                       outcome = "mpg",
-                       offset = "drat")
-  
+  task <- sl3_Task$new(mtcars,
+    covariates = c("disp", "hp", "wt"),
+    outcome = "mpg",
+    offset = "drat"
+  )
+
   ## instantiate Lrnr_glmtree, train on task, and predict on task
   lrnr_glmtree <- Lrnr_glmtree$new(formula = "mpg ~ drat + disp")
   expect_error(lrnr_glmtree$train(task))
 })
 
 test_that("Lrnr_glmtree errors when outcome in formula is misspecified", {
-  
-  task <- sl3_Task$new(mtcars, 
-                       covariates = c("disp", "hp",  "wt"), 
-                       outcome = "mpg",
-                       offset = "drat")
-  
+  task <- sl3_Task$new(mtcars,
+    covariates = c("disp", "hp", "wt"),
+    outcome = "mpg",
+    offset = "drat"
+  )
+
   ## instantiate Lrnr_glmtree, train on task, and predict on task
   lrnr_glmtree <- Lrnr_glmtree$new(formula = "hp ~ wt + disp")
   expect_error(lrnr_glmtree$train(task))
