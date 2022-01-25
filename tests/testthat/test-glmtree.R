f963ea973bbda7a27f1ade869c2186ab93caadd0
rachaelvp <rachaelvphillips@gmail.com>
2022-01-24 17:24:31 -0800

    test formula offset error

--- tests/testthat/test-glmtree.R
+++ tests/testthat/test-glmtree.R
@@ -52,17 +52,15 @@ test_that("Lrnr_glmtree includes offset correctly", {
   ## instantiate Lrnr_glmtree, train on task, and predict on task
   lrnr_glmtree <- Lrnr_glmtree$new(alpha = 0.9, prune = "AIC")
   fit_lrnr_glmtree <- lrnr_glmtree$train(task)
-  prd_lrnr_glmtree <- suppressWarnings(fit_lrnr_glmtree$predict())
+  prd_lrnr_glmtree <- fit_lrnr_glmtree$predict()
 
   # fit glmtree with same specification as sl3
-  # we need to set max_depth=10, since that was set as the default in
-  # Lrnr_glmtree and it is different from the partykit::glmtree default of Inf
   d <- task$data
-  fit_glmtree <- do.call(partykit::glmtree, list(
+  fit_glmtree <- partykit::glmtree(
     formula = as.formula(mpg ~ offset(drat) | disp + hp + wt),  data = d,
     alpha = 0.9, prune = "AIC"
-  ))
-  prd_glmtree <- suppressWarnings(predict(fit_glmtree, newdata = d))
+  )
+  prd_glmtree <- predict(fit_glmtree, newdata = d)
 
   ## test equivalence of prediction from Lrnr_glmtree and partykit::glmtree
   expect_equal(prd_lrnr_glmtree, as.numeric(prd_glmtree))
@@ -89,5 +87,17 @@ test_that("Lrnr_glmtree errors when outcome in formula is misspecified", {
 
   ## instantiate Lrnr_glmtree, train on task, and predict on task
   lrnr_glmtree <- Lrnr_glmtree$new(formula = "hp ~ wt + disp")
+  expect_error(lrnr_glmtree$train(task))
+})
+
+test_that("Lrnr_glmtree errors when offset not in user-specified formula", {
+  task <- sl3_Task$new(mtcars,
+                       covariates = c("disp", "hp", "wt"),
+                       outcome = "mpg",
+                       offset = "drat"
+  )
+  
+  ## instantiate Lrnr_glmtree, train on task, and predict on task
+  lrnr_glmtree <- Lrnr_glmtree$new(formula = "mpg ~ wt + disp + hp")
   expect_error(lrnr_glmtree$train(task))
 })
