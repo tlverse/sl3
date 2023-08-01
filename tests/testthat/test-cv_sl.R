8b3d7e027b9e89a82ad2b0168955c19912e30a90
rachaelvp <rachaelvphillips@gmail.com>
2023-07-31 19:04:45 -0700

    test cv_sl predictions

--- tests/testthat/test-cv_sl.R
+++ tests/testthat/test-cv_sl.R
@@ -9,17 +9,18 @@ test_that("cross-validated super learner works", {
   covars <- c("apgar1", "apgar5", "parity", "gagebrth", "mage", "meducyrs", "sexn")
   outcome <- "haz"
   task <- sl3_Task$new(data.table::copy(cpp_imputed),
-                       covariates = covars,
-                       outcome = outcome,
-                       folds = origami::make_folds(cpp_imputed, V = 3)
+    covariates = covars,
+    outcome = outcome,
+    folds = 3
   )
   glm_learner <- Lrnr_glm$new()
   glmnet_learner <- Lrnr_pkg_SuperLearner$new("SL.glmnet")
-  subset_apgar <- Lrnr_subset_covariates$new(covariates = c("apgar1", "apgar5"))
-  learners <- list(glm_learner, glmnet_learner, subset_apgar)
+  earth_learner <- Lrnr_earth$new()
+  learners <- list(glm_learner, glmnet_learner, earth_learner)
   sl <- make_learner(Lrnr_sl, learners, glm_learner)
   sl_fit <- sl$train(task)
   cv_sl_fit <- cv_sl(sl_fit, task, loss_squared_error)
-  
+
   expect_false(any(is.na(cv_sl_fit)))
+  expect_equal(nrow(cv_sl_fit$cv_preds), task$nrow)
 })
