330c91eefeeee32184116f12a58feada4b802756
rachaelvp <rachaelvphillips@gmail.com>
2023-07-31 19:04:21 -0700

    return cv predictions for sl and candidates

--- R/cv_sl.R
+++ R/cv_sl.R
@@ -47,7 +47,6 @@ cv_sl <- function(lrnr_sl, task, eval_fun) {
   cv_sl_lrnr <- make_learner(Lrnr_cv, lrnr_sl, full_fit = TRUE)
   cv_sl_fit <- cv_sl_lrnr$train(task)
   full_fit <- cv_sl_fit$fit_object$full_fit
-  # TODO: extract loss function from lrnr_sl where possible
   full_risk <- full_fit$cv_risk(eval_fun)
   sl_risk <- cv_sl_fit$cv_risk(eval_fun)
   # replace revere cv sl risk with nested cv sl risk as needed
@@ -58,13 +57,15 @@ cv_sl <- function(lrnr_sl, task, eval_fun) {
   # remove full-fit SL coefficients from the table
   risks <- risks[, -"coefficients", with = FALSE]
 
-  # gather the fold-specific SL coefficients
+  # gather the fold-specific SL coefficients and validation set predictions
   if (!is.null(names(full_fit$coefficients))) {
     coefs <- do.call(
       rbind,
       lapply(seq_along(cv_sl_fit$fit_object$fold_fits), function(i) {
-        coefs <- stats::coef(cv_sl_fit$fit_object$fold_fits[[i]]$fit_object$cv_meta_fit)
-        c("fold" = i, coefs)
+        sl_coefs_i <- stats::coef(
+          cv_sl_fit$fit_object$fold_fits[[i]]$fit_object$cv_meta_fit
+        )
+        c("fold" = i, sl_coefs_i)
       })
     )
   } else {
@@ -73,9 +74,36 @@ cv_sl <- function(lrnr_sl, task, eval_fun) {
     })
   }
 
+  cv_preds <- do.call(
+    rbind,
+    lapply(seq_along(cv_sl_fit$fit_object$fold_fits), function(i) {
+      # fold i validation task
+      valid_task_i <- cv_sl_fit$training_task$subset_task(
+        cv_sl_fit$training_task$folds[[i]]$validation_set
+      )
+      # candidate predictions for fold i validation task
+      cand_pred_i <- lapply(
+        seq_along(cv_sl_fit$fit_object$fold_fits[[i]]$learner_fits), function(k) {
+          cv_sl_fit$fit_object$fold_fits[[i]]$learner_fits[[k]]$predict(valid_task_i)
+        }
+      )
+      cand_pred_i <- data.table(do.call(cbind, cand_pred_i))
+      colnames(cand_pred_i) <- names(cv_sl_fit$fit_object$fold_fits[[i]]$learner_fits)
+
+      # SL predictions for fold i validation task (NOT SEEN BY SL)
+      sl_pred_i <- cv_sl_fit$fit_object$fold_fits[[1]]$predict(valid_task_i)
+
+      data.table(
+        "row_index" = cv_sl_fit$training_task$folds[[i]]$validation_set,
+        "SuperLearner" = sl_pred_i,
+        cand_pred_i
+      )
+    })
+  )
+  cv_preds <- cv_preds[order(row_index), -"row_index", with = F]
+
   print("Cross-validated risk:")
   print(risks)
 
-  return_obj <- list("coef" = coefs, "cv_risk" = risks)
-  return(return_obj)
+  return(list("cv_risk" = risks, "cv_sl_coefs" = coefs, "cv_preds" = cv_preds))
 }
